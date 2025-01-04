430ad64e805424172dfc47e2e21a9d5cfa0f6c03
mguittari <mattias.guittari@gmail.com>
2025-01-04 23:11:38 +0100

    toggle button for changing theme (red/blue)

--- src/Header/ButtonColorTheme.jsx
+++ src/Header/ButtonColorTheme.jsx
@@ -4,13 +4,12 @@ import { ThemeContext } from "../Context/ThemeContext";
 export default function ButtonColorTheme() {
 	const { colorTheme, setColorTheme } = useContext(ThemeContext);
 	return (
-		<>
+		<div className="button-flex">
 			<button
 				type="button"
+				className={`button-toggle-theme ${colorTheme === "Blue" ? "button-toggle-theme-red" : "button-toggle-theme-blue"} `}
 				onClick={() => setColorTheme(colorTheme === "Blue" ? "Red" : "Blue")}
-			>
-				Color
-			</button>
-		</>
+			/>
+		</div>
 	);
 }
