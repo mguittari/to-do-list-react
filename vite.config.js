82c8ce81186fe4f4d03cd8247a6579cc3f968100
mguittari <mattias.guittari@gmail.com>
2025-01-05 22:42:39 +0100

    vite config and package json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "/to-do-list-react/", // Remplace par le nom de ton dépôt GitHub
+	build: {
+		outDir: "dist", // Si nécessaire, sinon garde 'dist' (par défaut)
+	},
+});
