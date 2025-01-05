e7f3d3202e2f0ee19bbebe98605a980d540ee800
mguittari <mattias.guittari@gmail.com>
2025-01-05 23:26:23 +0100

    readme updated | vite config updated

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 ![Red version](/src/assets/todolist-2.png)
 
+V2 disponible ici : https://mguittari.github.io/to-do-list-react/
+
+NB: Il y a un bug quand on recharge la page avec une ou plusieurs tâches rayées.
+Je m'en occupe dès que possible. Sinon tout fonctionne.
 
 ## V1
 - Crée une tâche
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/to-do-list-react/", // Remplace par le nom de ton dépôt GitHub
+	base: "/to-do-list-react/", // Chemin relatif pour le déploiement sur GitHub Pages
 	plugins: [react()],
+	build: {
+		// Options de build
+		outDir: "dist", // Répertoire de sortie pour la build
+	},
 });
