82fa44cff30a8e7b48a0956764c22bc88559e180
mguittari <mattias.guittari@gmail.com>
2025-01-05 17:48:22 +0100

    stock the theme in localstorage

--- src/Header/ButtonColorTheme.jsx
+++ src/Header/ButtonColorTheme.jsx
@@ -1,14 +1,27 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ThemeContext } from "../Context/ThemeContext";
 
 export default function ButtonColorTheme() {
 	const { colorTheme, setColorTheme } = useContext(ThemeContext);
+
+	useEffect(() => {
+		const storedTheme = localStorage.getItem("Color theme");
+		if (storedTheme) {
+			setColorTheme(storedTheme); // Met à jour le contexte avec le thème stocké
+		}
+	}, [setColorTheme]);
+
+	const handleClick = () => {
+		const newTheme = colorTheme === "Blue" ? "Red" : "Blue";
+		setColorTheme(newTheme);
+		localStorage.setItem("Color theme", newTheme); // Met à jour le localStorage
+	};
 	return (
 		<div className="button-flex">
 			<button
 				type="button"
 				className={`button-toggle-theme ${colorTheme === "Blue" ? "button-toggle-theme-red" : "button-toggle-theme-blue"} `}
-				onClick={() => setColorTheme(colorTheme === "Blue" ? "Red" : "Blue")}
+				onClick={handleClick}
 			/>
 		</div>
 	);
