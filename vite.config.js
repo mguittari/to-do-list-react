052974e3d654b9bccb18328d90347aa42724f0a8
mguittari <mattias.guittari@gmail.com>
2025-01-05 23:03:46 +0100

    main updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	base: "/to-do-list-react/", // Remplace par le nom de ton dépôt GitHub
-	build: {
-		outDir: "dist", // Si nécessaire, sinon garde 'dist' (par défaut)
-	},
 });
